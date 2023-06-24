634c274e1a244523b770021926ac426813f0eab9
yukilun <102889935+yukilun@users.noreply.github.com>
2023-06-23 21:34:03 -0700

    Add Toast message after verifying email

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -16,17 +16,21 @@ class VerifyEmailController extends Controller
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+            return redirect()->intended(RouteServiceProvider::HOME . '?verified=1')
+                ->with('message', [
+                    'type' => 'success',
+                    'message' => 'Your email has already been verified.'
+                ]);
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1')
-                        ->with('message', [
-                            'type' => 'success',
-                            'message' => 'Your email has been verified!'
-                        ]);
+        return redirect()->intended(RouteServiceProvider::HOME . '?verified=1')
+            ->with('message', [
+                'type' => 'success',
+                'message' => 'Your email has been verified!'
+            ]);
     }
 }
